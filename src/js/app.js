bed86995cae0bafdad431f26d486d0e7f2112e0e
RoxanaCS <roxale.cares@gmail.com>
2018-02-06 12:42:46 -0300

    encontrar posición

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,39 @@
- $(document).ready(function() {
-    Materialize.updateTextFields();
+$(document).ready(function() {
+  Materialize.updateTextFields();
+});
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397,
+      lng: 150.644},
+    zoom: 18
   });
 
- function initMap() {
-        var test = {lat: -12, lng: -77};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: test
-        });
-        var marker = new google.maps.Marker({
-          position: test,
-          map: map
-        });
-      }
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+  // Try HTML5 geolocation.
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+  // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+}
