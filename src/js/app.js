6ff2cae7feb2e1197ed821ff15c4ba9efa0f6d23
Tanko <tankodani@gmail.com>
2018-02-06 11:20:22 -0300

    agregando mapa

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,15 @@
  $(document).ready(function() {
     Materialize.updateTextFields();
   });
+
+ function initMap() {
+        var test = {lat: -12, lng: -77};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: test
+        });
+        var marker = new google.maps.Marker({
+          position: test,
+          map: map
+        });
+      }
