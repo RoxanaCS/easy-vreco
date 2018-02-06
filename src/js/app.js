f191ed48f59b18e7f57cc46ab5e04cba8fe2fcb6
Tanko <tankodani@gmail.com>
2018-02-06 12:43:01 -0300

    autocomplete

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,24 @@
- $(document).ready(function() {
-    Materialize.updateTextFields();
-  });
+$(document).ready(function() {
+  Materialize.updateTextFields();
+});
 
- function initMap() {
-        var test = {lat: -12, lng: -77};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: test
-        });
-        var marker = new google.maps.Marker({
-          position: test,
-          map: map
-        });
-      }
+function initMap() {
+  var test = {lat: -12, lng: -77};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: test
+  });
+  var marker = new google.maps.Marker({
+    position: test,
+    map: map
+  });
+  activateSearch()
+}
+
+function activateSearch() {
+  var startInput = document.getElementById('startInput');
+  var destinationInput = document.getElementById('destinationInput');
+
+  new google.maps.places.Autocomplete(startInput);
+  new google.maps.places.Autocomplete(destinationInput);
+}
